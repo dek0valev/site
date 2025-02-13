af6d4774bb45acf59f705f7748c382a909a8dd4c
dek0valev <dek0valev@proton.me>
2025-02-13 12:35:32 +0700

    feat: create `blog` page

---
+++ src/pages/articles-list/index.ts
@@ -0,0 +1,1 @@
+export { ArticlesListPage } from './ui/page';
---
+++ src/pages/articles-list/ui/page.tsx
@@ -0,0 +1,32 @@
+import { fetchArticles } from '@/pages/articles-list/api/fetch-articles';
+import { ArticleCard } from '@/pages/articles-list/ui/article-card';
+
+export async function ArticlesListPage() {
+    const articles = await fetchArticles();
+
+    return (
+        <section>
+            <div className="py-10 lg:py-16">
+                <div className="container max-w-screen-sm">
+                    <div className="flex flex-col">
+                        <h1 className="mb-5 text-xl font-medium text-neutral-900 dark:text-white">Блог</h1>
+                        <div className="grid auto-rows-auto grid-cols-1 gap-y-3">
+                            {articles.map(({ slug, category, title, publishedAt }) => {
+                                return (
+                                    <div key={slug} className="inline-flex flex-col">
+                                        <ArticleCard
+                                            category={category}
+                                            title={title}
+                                            publishedAt={publishedAt}
+                                            href={`/blog/${slug}`}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
