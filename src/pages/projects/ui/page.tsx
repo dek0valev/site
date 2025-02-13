ab38f02dbdddbb93256b3c1192a072f49bb94252
dek0valev <dek0valev@proton.me>
2025-02-13 13:19:51 +0700

    feat: create `projects` page

---
+++ src/pages/projects/ui/page.tsx
@@ -0,0 +1,14 @@
+export function ProjectsPage() {
+    return (
+        <section>
+            <div className="py-10 lg:py-16">
+                <div className="container max-w-screen-md">
+                    <div className="flex flex-col">
+                        <h1 className="mb-5 text-2xl font-medium text-neutral-900 dark:text-white">Проекты</h1>
+                        <p>...</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
